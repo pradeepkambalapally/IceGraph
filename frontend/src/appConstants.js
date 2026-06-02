0ee7c3932be1db0ab7df7fc16422bcf646de798f
YanivZalach <yzal2318@gmail.com>
2026-06-02 16:04:48 +0300

    Fix demo

--- frontend/src/appConstants.js
+++ frontend/src/appConstants.js
@@ -1,2 +1,2 @@
-export const MOCK_HOME = '/table/graph?table=default.events'
+export const MOCK_HOME = '/IceGraph/table/graph?table=default.events'
 export const IS_MOCK = import.meta.env.VITE_USE_MSW === 'true'
